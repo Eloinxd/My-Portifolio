96716fdf754fbf6dcfd45a1ef61f9b6bfa967780
PedroPCouto <pedrohpaiva1258@gmail.com>
2023-03-09 21:32:43 -0300

    Styling and animating home page

---
+++ src/components/Home/home.component.jsx
@@ -0,0 +1,29 @@
+import './home.styles.scss';
+import LogoTitle from '../../assets/images/logo-s.png';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = ['e','d','r','o'];
+    const jobArray = ['W', 'e', 'b', 'S', 'a', 'l', 'e', 's', 'f', 'o','r','c','e', 'D','e','v','e','l','o','p','e','r'];
+    return(
+        <div className='container home-page'>
+            <div className='text-zone'>
+                <h1>Hi, I'm<br />
+                    <img src={LogoTitle} alt="P" />
+                     edro
+                    <br />
+                    Web / Salesforce Developer
+                </h1>
+                <div className='contact-text'>
+                    <h2>
+                        FullStack Developer / TypeScript enthusiast
+                    </h2>
+                    <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home; 
